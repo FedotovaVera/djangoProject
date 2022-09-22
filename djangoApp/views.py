d38f92e4b237ca883603aa68277a4ffef1f18e26
Vera <verafedotova912016@gmail.com>
2022-09-22 15:39:57 +0300

    +

--- djangoApp/views.py
+++ djangoApp/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.db.models import Q
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
@@ -11,33 +12,35 @@ from django.views.generic import DetailView, UpdateView, CreateView
 
 
 def hello(request):
+    user_id = User.objects.all().filter(id=request.user.id)[0].id
+
     dt1 = datemanager.DateManager().day_monday.day
     dt_tsk_1 = datemanager.DateManager().day_monday.strftime("%Y-%m-%d")
-    tsk1 = Tasks.objects.all().order_by('time_task').filter(date_end='', date_task=dt_tsk_1)
+    tsk1 = Tasks.objects.all().order_by('time_task').filter(date_end='', date_task=dt_tsk_1, user_id_id=user_id)
 
     dt2 = datemanager.DateManager().day_tuesday.day
     dt_tsk_2 = datemanager.DateManager().day_tuesday.strftime("%Y-%m-%d")
-    tsk2 = Tasks.objects.all().order_by('time_task').filter(date_end='', date_task=dt_tsk_2)
+    tsk2 = Tasks.objects.all().order_by('time_task').filter(date_end='', date_task=dt_tsk_2, user_id_id=user_id)
 
     dt3 = datemanager.DateManager().day_wednesday.day
     dt_tsk_3 = datemanager.DateManager().day_wednesday.strftime("%Y-%m-%d")
-    tsk3 = Tasks.objects.all().order_by('time_task').filter(date_end='', date_task=dt_tsk_3)
+    tsk3 = Tasks.objects.all().order_by('time_task').filter(date_end='', date_task=dt_tsk_3, user_id_id=user_id)
 
     dt4 = datemanager.DateManager().day_thursday.day
     dt_tsk_4 = datemanager.DateManager().day_thursday.strftime("%Y-%m-%d")
-    tsk4 = Tasks.objects.all().order_by('time_task').filter(date_end='', date_task=dt_tsk_4)
+    tsk4 = Tasks.objects.all().order_by('time_task').filter(date_end='', date_task=dt_tsk_4, user_id_id=user_id)
 
     dt5 = datemanager.DateManager().day_friday.day
     dt_tsk_5 = datemanager.DateManager().day_friday.strftime("%Y-%m-%d")
-    tsk5 = Tasks.objects.all().order_by('time_task').filter(date_end='', date_task=dt_tsk_5)
+    tsk5 = Tasks.objects.all().order_by('time_task').filter(date_end='', date_task=dt_tsk_5, user_id_id=user_id)
 
     dt6 = datemanager.DateManager().day_saturday.day
     dt_tsk_6 = datemanager.DateManager().day_saturday.strftime("%Y-%m-%d")
-    tsk6 = Tasks.objects.all().order_by('time_task').filter(date_end='', date_task=dt_tsk_6)
+    tsk6 = Tasks.objects.all().order_by('time_task').filter(date_end='', date_task=dt_tsk_6, user_id_id=user_id)
 
     dt7 = datemanager.DateManager().day_sunday.day
     dt_tsk_7 = datemanager.DateManager().day_sunday.strftime("%Y-%m-%d")
-    tsk7 = Tasks.objects.all().order_by('time_task').filter(date_end='', date_task=dt_tsk_7)
+    tsk7 = Tasks.objects.all().order_by('time_task').filter(date_end='', date_task=dt_tsk_7, user_id_id=user_id)
 
     context = {'day_1': dt1, 'day_2': dt2, 'day_3': dt3, 'day_4': dt4, 'day_5': dt5, 'day_6': dt6, 'day_7': dt7,
                'tsk_1': tsk1, 'tsk_2': tsk2, 'tsk_3': tsk3, 'tsk_4': tsk4, 'tsk_5': tsk5, 'tsk_6': tsk6, 'tsk_7': tsk7}
